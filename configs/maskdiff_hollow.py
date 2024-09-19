97a0535485959a4dcc4450a8243acd9af88b98ad
yixiuz <yixiuz@stanford.edu>
2024-09-19 05:03:59 +0000

    Figured out how to use ckpt_restore_dir

--- configs/maskdiff_hollow.py
+++ configs/maskdiff_hollow.py
@@ -26,7 +26,8 @@ def get_config():
   config = ml_collections.ConfigDict()
   config.exp_name = "exp_vdm"
   config.model_type = "model_transformer"
-  config.ckpt_restore_dir = 'None'
+  # config.ckpt_restore_dir = 'None'
+  config.ckpt_restore_dir = 'gs://maskdiff/hollow/240918/checkpoints-0'
 
   config.use_hollow_transformer = True
 
