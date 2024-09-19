39a2cde0c373c5d9a4bbd675debdb209e155a735
yixiuz <yixiuz@stanford.edu>
2024-09-19 05:18:15 +0000

    Changed config

--- configs/maskdiff_hollow.py
+++ configs/maskdiff_hollow.py
@@ -76,7 +76,7 @@ def get_config():
       seed=1,
       substeps=1,
       num_steps_lr_warmup=100,
-      num_steps_train=500_000 / 2, #100_000_000,
+      num_steps_train=500_000, #100_000_000,
       num_steps_eval=100,
       batch_size_train=768 * 2, #1024 in paper version
       batch_size_eval=1024,
