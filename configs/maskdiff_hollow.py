f9e17f854570b167190e1395385d155f2890caf6
yixiuz <yixiuz@stanford.edu>
2024-09-24 22:02:46 +0000

    Trying out 256 steps without correctors

--- configs/maskdiff_hollow.py
+++ configs/maskdiff_hollow.py
@@ -57,11 +57,11 @@ def get_config():
 
   config.sampler = d(
     seed=42,
-    num_steps=50, # Cut the number of steps in half due to using correctors
+    num_steps=256, # Cut the number of steps in half due to using correctors
     max_samples=10_000,
-    output_file_name="barker_samples_10k",
+    output_file_name="no_corrector_256steps_samples_10k",
     # corrector="forward_backward",
-    corrector="barker",
+    corrector=None,#"barker",
     corrector_step_size=1.,
   )
 
