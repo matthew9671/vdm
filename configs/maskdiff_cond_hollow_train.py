9b510981c97e9fed297a0187b75733579a226c17
yixiuz <yixiuz@stanford.edu>
2024-09-28 00:01:31 +0000

    Resuming training with checkpoint

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -26,7 +26,8 @@ def get_config():
   config = ml_collections.ConfigDict()
   config.exp_name = "exp_vdm"
   config.model_type = "model_transformer"
-  config.ckpt_restore_dir = 'None'
+  # config.ckpt_restore_dir = 'None'
+  config.ckpt_restore_dir = 'gs://maskdiff/cond_hollow/240927/checkpoint-0/'
 
   config.use_hollow_transformer = True
 
