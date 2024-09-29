852b168180d6a944d18b24b5df2f5ab66122debf
yixiuz <yixiuz@stanford.edu>
2024-09-29 01:02:04 +0000

    Trying even larger stepsizes

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -59,7 +59,7 @@ def get_config():
     
     output_file_name="mpf_256steps_late_entry",
     # corrector=None,
-    corrector="mpf", corrector_step_size=.05,
+    corrector="mpf", corrector_step_size=.1,
     # corrector="barker", corrector_step_size=2.,
     # corrector="forward_backward", corrector_step_size=2.,
     corrector_entry_time=0.5,
