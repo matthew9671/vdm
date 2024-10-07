778aafcf50e64ebfc57d501eb48d0189b5234f4c
yixiuz <yixiuz@stanford.edu>
2024-10-07 23:34:45 +0000

    Testing mpf with early entry

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -58,11 +58,11 @@ def get_config():
     max_samples=128,
     update_type="euler", # "tau_leaping", "gillespies", "euler"
     
-    output_file_name="1fb_128psteps",
+    output_file_name="1mpf_early_128psteps",
     # corrector=None,
-    # corrector="mpf", corrector_step_size=.05,
+    corrector="mpf", corrector_step_size=.02,
     # corrector="barker", corrector_step_size=2.,
-    corrector="forward_backward", corrector_step_size=2.,
+    # corrector="forward_backward", corrector_step_size=2.,
     corrector_entry_time=0.9,
     num_corrector_steps=1,
 
