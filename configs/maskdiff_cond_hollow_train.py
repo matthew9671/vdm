5c7cf7233fe18cf9d893c1be041689e64a8f6cdf
yixiuz <yixiuz@stanford.edu>
2024-10-08 22:45:30 +0000

    Trying barker with higher stepsize and 128 steps

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -53,10 +53,10 @@ def get_config():
 
   config.sampler = d(
     seed=42,
-    num_steps=32, # Cut the number of steps in half due to using correctors
+    num_steps=128, # Cut the number of steps in half due to using correctors
     max_samples=10_000, update_type="euler", # "tau_leaping", "gillespies", "euler", "test_convergence"
     # max_samples=128, update_type="test_convergence",
-    output_file_name="1barker_128psteps",
+    output_file_name="1barker_128psteps_large",
     # corrector=None,
     # corrector="mpf", corrector_step_size=.01,
     corrector="barker", corrector_step_size=5.,
