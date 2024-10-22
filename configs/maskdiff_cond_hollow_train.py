dad34c2e84b64c811532176e4db6f46aa42c3f75
yixiuz <yixiuz@stanford.edu>
2024-10-22 00:01:12 +0000

    .

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -56,12 +56,12 @@ def get_config():
     num_steps=16, # Cut the number of steps in half due to using correctors
     max_samples=10_000, update_type="euler", # "tau_leaping", "gillespies", "euler", "test_convergence"
     # max_samples=128, update_type="test_convergence",
-    tag="restricted",
+    tag="",
     # corrector=None,
-    corrector="mpf", corrector_step_size=.02,
+    corrector="mpf", corrector_step_size=.04,
     # corrector="barker", corrector_step_size=4.,
     # corrector="forward_backward", corrector_step_size=4.,
-    corrector_entry_time=0.9,
+    corrector_entry_time=0.5,
     num_corrector_steps=1,
 
     # Testing corrector convergence
