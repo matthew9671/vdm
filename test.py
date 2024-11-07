fa013c62f2b3ea5e0c2e6691c3e2f1dd57cfe7a5
matthew9671 <yixiuz@stanford.edu>
2024-11-07 23:11:08 +0000

    .

--- test.py
+++ test.py
@@ -20,26 +20,26 @@ print(f"What is going on? Says process {jax.process_index()}")
 
 if jax.process_index() == 0:
 
-    logging.info('global device count:', jax.device_count())
-    logging.info('local device count:', jax.local_device_count())
-    logging.info('pmap result:', r)
+    print('global device count:', jax.device_count())
+    print('local device count:', jax.local_device_count())
+    print('pmap result:', r)
     
-    logging.info("Worker " + socket.gethostname() + " has process id 0.")
+    print("Worker " + socket.gethostname() + " has process id 0.")
 
     # weights = '/home/yixiuz/fid/inception_v3_weights_fid.pickle?dl=1'
     # reference = '/home/yixiuz/fid/VIRTUAL_imagenet256_labeled.npz'
     # fid = fidjax.FID(weights, reference)
       
     # all_acts = jnp.load("/home/yixiuz/logs/samples/16psteps_1mpf_size=0.04_late_entry_acts.npy", allow_pickle=True)      
-    # logging.info("Loaded activations")
+    # print("Loaded activations")
     
     # stats = fid.compute_stats(all_acts[:1000])
-    # logging.info("Computed stats")
+    # print("Computed stats")
     
     # # We have to move these to the cpu since matrix sqrt is not supported by tpus yet
     # stats_cpu = jax.device_put(stats, device=jax.devices("cpu")[0])
     # ref_cpu = jax.device_put(fid.ref, device=jax.devices("cpu")[0])
-    # logging.info("Put the arrays on the cpu")
+    # print("Put the arrays on the cpu")
     
     # score = fid.compute_score(stats_cpu, ref_cpu)
-    # logging.info(f"FID score: {score}")
+    # print(f"FID score: {score}")
