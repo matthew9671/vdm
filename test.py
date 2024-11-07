eb4d5d1f97359464a20263b423029658ecc65ddf
matthew9671 <yixiuz@stanford.edu>
2024-11-07 23:00:28 +0000

    .

--- test.py
+++ test.py
@@ -2,17 +2,17 @@
 import jax
 import socket
 
-# # The total number of TPU cores in the Pod
-# device_count = jax.device_count()
+# The total number of TPU cores in the Pod
+device_count = jax.device_count()
 
-# # The number of TPU cores attached to this host
-# local_device_count = jax.local_device_count()
+# The number of TPU cores attached to this host
+local_device_count = jax.local_device_count()
 
-# # The psum is performed over all mapped devices across the Pod
-# xs = jax.numpy.ones(jax.local_device_count())
-# r = jax.pmap(lambda x: jax.lax.psum(x, 'i'), axis_name='i')(xs)
+# The psum is performed over all mapped devices across the Pod
+xs = jax.numpy.ones(jax.local_device_count())
+r = jax.pmap(lambda x: jax.lax.psum(x, 'i'), axis_name='i')(xs)
 
-# # Print from a single host to avoid duplicated output
+# Print from a single host to avoid duplicated output
 
 import fidjax
 import jax.numpy as jnp
