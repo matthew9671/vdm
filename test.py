a44a344c7781338971dc518f0cc2a3c11ce78199
matthew9671 <yixiuz@stanford.edu>
2024-11-07 22:24:28 +0000

    Trying to debug fid.compute_score

--- test.py
+++ test.py
@@ -1,25 +1,43 @@
-print("Hello world!")
+# print("Hello world!")
 
-# The following code snippet will be run on all TPU hosts
-import jax
-import socket
+# # The following code snippet will be run on all TPU hosts
+# import jax
+# import socket
 
-print("Imported jax successfully")
+# print("Imported jax successfully")
 
-# The total number of TPU cores in the Pod
-device_count = jax.device_count()
+# # The total number of TPU cores in the Pod
+# device_count = jax.device_count()
 
-# The number of TPU cores attached to this host
-local_device_count = jax.local_device_count()
+# # The number of TPU cores attached to this host
+# local_device_count = jax.local_device_count()
 
-# The psum is performed over all mapped devices across the Pod
-xs = jax.numpy.ones(jax.local_device_count())
-r = jax.pmap(lambda x: jax.lax.psum(x, 'i'), axis_name='i')(xs)
+# # The psum is performed over all mapped devices across the Pod
+# xs = jax.numpy.ones(jax.local_device_count())
+# r = jax.pmap(lambda x: jax.lax.psum(x, 'i'), axis_name='i')(xs)
 
-# Print from a single host to avoid duplicated output
-if jax.process_index() == 0:
-    print('global device count:', jax.device_count())
-    print('local device count:', jax.local_device_count())
-    print('pmap result:', r)
+# # Print from a single host to avoid duplicated output
+# if jax.process_index() == 0:
+#     print('global device count:', jax.device_count())
+#     print('local device count:', jax.local_device_count())
+#     print('pmap result:', r)
     
-    print("Worker " + socket.gethostname() + " has process id 0.")
+#     print("Worker " + socket.gethostname() + " has process id 0.")
+
+import jax
+import fidjax
+
+if jax.process_index() == 0:
+    weights = '/home/yixiuz/fid/inception_v3_weights_fid.pickle?dl=1'
+    reference = '/home/yixiuz/fid/VIRTUAL_imagenet256_labeled.npz'
+    fid = fidjax.FID(weights, reference)
+      
+    all_acts = jnp.load("/home/yixiuz/logs/samples/16psteps_1mpf_size=0.04_late_entry_acts.npy", allow_pickle=True)      
+    stats = fid.compute_stats(all_acts)
+    # We have to move these to the cpu since matrix sqrt is not supported by tpus yet
+    stats_cpu = jax.device_put(stats, device=jax.devices("cpu")[0])
+    ref_cpu = jax.device_put(fid.ref, device=jax.devices("cpu")[0])
+      
+    
+    score = fid.compute_score(stats_cpu, ref_cpu)
+    print(f"FID score: {score}")
