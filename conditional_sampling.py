777c0bdc2e7ee03cfea9586c7bdc7da9dc141c59
matthew9671 <yixiuz@stanford.edu>
2024-11-22 00:18:51 +0000

    Reverted commit and testing using logits instead of mpf rates, testing K=4 again

--- conditional_sampling.py
+++ conditional_sampling.py
@@ -253,6 +253,7 @@ def mask_conditional_k_gillespies_update_mpf(key, x, x0_logits, mask=1024, k=1):
     # Find which locations each dimension would transition to conditioning on a transition
     # It seems that it doesn't matter whether we include the self-transition or not
     # jump_target = jr.categorical(key_cat, jnp.log(rates + eps)).astype(jnp.int32)
+    logits = x0_logits.at[:, mask].set(-jnp.inf)
     jump_target = jr.categorical(key_cat, logits).astype(jnp.int32)
 
     taus_sorted = jnp.sort(taus, axis=-1)
