dce648b89170e74d944b0c15dab7b03b02e07785
matthew9671 <yixiuz@stanford.edu>
2024-12-06 01:54:39 +0000

    Minor fix

--- conditional_sampling.py
+++ conditional_sampling.py
@@ -34,9 +34,9 @@ def compute_backward(y_with_label, t, apply_fn, params, config, forward_process)
     x0_logits = x0_logits[0,1:-1,:S]
     
     # # Set mask logits to minus infinity and normalize
-    # x0_logits = jnp.where(y == mask, -jnp.inf, x0_logits)
-    # x0_logits -= jax.scipy.special.logsumexp(x0_logits, axis=-1, 
-    #     keepdims=True)
+    x0_logits = jnp.where(y == mask, -jnp.inf, x0_logits)
+    x0_logits -= jax.scipy.special.logsumexp(x0_logits, axis=-1, 
+        keepdims=True)
 
     # p^{*1}_{0|t}(*2|y): (D, S) float array of marginal likelihoods for each dimension predicted by the model
     p0t_eval_y = softmax(x0_logits, axis=-1)
