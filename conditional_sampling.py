6c9c20d27b57ba327c2a46874fea6f4b3768b769
matthew9671 <yixiuz@stanford.edu>
2025-01-04 23:42:24 +0000

    testing ratio=.15

--- conditional_sampling.py
+++ conditional_sampling.py
@@ -380,7 +380,7 @@ def backward_process_gibbs(apply_fn, params, ts, config, xT, key, forward_proces
         rp = res["rates"]
         update = update_func(p_key, x[1:-1], rp * dt)
         # Figure out the number of changed dimension and setting k accordingly
-        k = jnp.floor(jnp.sum(x[1:-1] != update) * 0.2).astype(int)
+        k = jnp.floor(jnp.sum(x[1:-1] != update) * 0.15).astype(int)
         x = x.at[1:-1].set(update)
 
         # Change current time (!!)
