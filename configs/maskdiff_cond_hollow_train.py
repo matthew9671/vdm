2588f4edbd1c8c3ccf441f3dce9cf3062b3f3b2f
matthew9671 <yixiuz@stanford.edu>
2025-01-04 23:17:02 +0000

    8 step results not too good, trying 16 steps

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -53,12 +53,12 @@ def get_config():
 
   config.sampler = d(
     seed=42,
-    num_steps=8, # Cut the number of steps in half due to using correctors
+    num_steps=16, # Cut the number of steps in half due to using correctors
     max_samples=10_000, 
     # "tau_leaping", "gillespies", "euler", "gibbs", "test_convergence"
     update_type="gibbs", 
     # max_samples=128, update_type="test_convergence",
-    tag="zero_min_t",
+    tag="adaptive_k",
     corrector="gibbs", corrector_step_size=0,
     # corrector="gibbs_uninformed", corrector_step_size=0,
     # corrector="gibbs_mpf", corrector_step_size=0,
@@ -71,8 +71,8 @@ def get_config():
     # Testing corrector convergence
     # predictor_cutoff_time=0.25, convergence_steps=100,
     # Only update masked tokens at the last argmax step
-    restricted=False,#True
-    k = 8,
+    restricted=False, # True
+    k = 0,
     top_k_temperature=0.
   )
 
