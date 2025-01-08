50b9ac5fe04f655a794b56277d786623e177206c
matthew9671 <yixiuz@stanford.edu>
2025-01-08 04:45:13 +0000

    Adding more temperature

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -74,7 +74,7 @@ def get_config():
     # If set to true, only update masked tokens at the last argmax step
     restricted=False,
     k = 16,
-    top_k_temperature=2.5,
+    top_k_temperature=3.,
     maskgit_temperature=6,
   )
 
