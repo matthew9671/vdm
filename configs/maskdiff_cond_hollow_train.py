7e32ab76b75f0ceb20562d3e1767e50b70761da9
yixiuz <yixiuz@stanford.edu>
2025-01-20 03:19:40 +0000

    Added rotary embeddings

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -18,18 +18,18 @@ import subprocess
 
 def get_git_commit_message():
   try:
-      # Run the Git command to get the latest commit message
-      result = subprocess.run(
-          ["git", "log", "-1", "--pretty=%B"],
-          stdout=subprocess.PIPE,
-          stderr=subprocess.PIPE,
-          text=True,
-          check=True,
-      )
-      return result.stdout.strip()  # Return the commit message
+    result = subprocess.run(
+        ["git", "log", "-1", "--pretty=%B"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True,
+        cwd="/home/yixiuz/vdm/",
+    )
+    return result.stdout.strip()  # Return the commit message
   except subprocess.CalledProcessError as e:
-      print("Error retrieving Git commit message:", e.stderr)
-      return None
+    print("Error retrieving Git commit message:", e.stderr)
+    return None
 
 
 def d(**kwargs):
--- transformer.py
+++ transformer.py
@@ -146,6 +146,7 @@ class Embed(nn.Module):
   max_position_embeddings: int
   initializer_fn: InitializerType
   hidden_size: Optional[int] = None
+  use_position_embeddings: bool = True
 
   @nn.compact
   def __call__(self, input_ids: jnp.ndarray,
@@ -286,7 +287,113 @@ class Transformer(nn.Module):
 
 # ---------------------------------------------------------------------
 # Hollow transformer implementation based on the maskgit implementation
+# 2025/1/19 Update: added rotary position encoding from MD4
 # ---------------------------------------------------------------------
+
+def precompute_freqs_cis(dim, end, theta: float = 10000.0):
+  freqs = 1.0 / (theta ** (jnp.arange(0, dim, 2)[: (dim // 2)] / dim))
+  t = jnp.arange(end)
+  freqs = jnp.outer(t, freqs)
+  freqs_cos = jnp.cos(freqs)
+  freqs_sin = jnp.sin(freqs)
+  return freqs_cos, freqs_sin
+
+def apply_rotary_emb(x, freqs_cos, freqs_sin):
+  # reshape x to match the complex representation
+  # [bs, seq_len, n_head, head_dim // 2]
+  x_r, x_i = jax_unstack(x.reshape(x.shape[:-1] + (-1, 2)), -1)
+
+  # reshape freqs_cos and freqs_sin for broadcasting
+  # [1, seq_len, 1, head_dim // 2]
+  freqs_cos = reshape_for_broadcast(freqs_cos, x_r)
+  freqs_sin = reshape_for_broadcast(freqs_sin, x_r)
+
+  # apply rotation using real numbers
+  x_out_r = x_r * freqs_cos - x_i * freqs_sin
+  x_out_i = x_r * freqs_sin + x_i * freqs_cos
+
+  # flatten last two dimensions
+  # [bs, seq_len, n_head, head_dim // 2, 2] -> [bs, seq_len, n_head, head_dim]
+  x_out = jnp.stack([x_out_r, x_out_i], axis=-1).reshape(
+      x_out_r.shape[:3] + (-1,)
+  )
+  return x_out
+
+class MaskedAttentionWithRoPE(nn.Module):
+  """
+  Attention that takes in an additive mask, with the "causal" flag removed.
+  Everything else is identical with the original MD4 implementation
+  """
+
+  dim: int
+  n_heads: int
+  n_kv_heads: int | None = None
+  dropout_rate: float = 0.0
+  qkv_bias: bool = True # In line with flax implementation
+
+  def setup(self):
+    self._n_kv_heads = (
+        self.n_heads if self.n_kv_heads is None else self.n_kv_heads
+    )
+    assert self.n_heads % self._n_kv_heads == 0
+    self.n_rep = self.n_heads // self._n_kv_heads
+    self.head_dim = self.dim // self.n_heads
+    self.wq = nn.Dense(self.n_heads * self.head_dim, use_bias=self.qkv_bias)
+    self.wk = nn.Dense(self._n_kv_heads * self.head_dim, use_bias=self.qkv_bias)
+    self.wv = nn.Dense(self._n_kv_heads * self.head_dim, use_bias=self.qkv_bias)
+    self.wo = nn.Dense(self.dim, use_bias=False)
+    if self.dropout_rate > 0.0:
+      self.attn_dropout = nn.Dropout(self.dropout_rate)
+      self.resid_dropout = Dropout1d(self.dropout_rate)
+
+  def __call__(self, x_q, x_kv, attn_mask, freqs_cos_q, freqs_sin_q, 
+    freqs_cos_kv=None, freqs_sin_kv=None, deterministic=True):
+    bsz, seqlen_q, _ = x_q.shape
+    _, seqlen_kv, _ = x_kv.shape
+
+    # QKV
+    xq, xk, xv = self.wq(x_q), self.wk(x_kv), self.wv(x_kv)
+    xq = xq.reshape(bsz, seqlen_q, self.n_heads, self.head_dim)
+    xk = xk.reshape(bsz, seqlen_kv, self._n_kv_heads, self.head_dim)
+    xv = xv.reshape(bsz, seqlen_kv, self._n_kv_heads, self.head_dim)
+
+    # RoPE relative positional embeddings
+    xq = apply_rotary_emb(xq, freqs_cos_q, freqs_sin_q)
+    
+    freqs_cos_kv = freqs_cos_q if freqs_cos_kv is None else freqs_cos_kv
+    freqs_sin_kv = freqs_sin_q if freqs_sin_kv is None else freqs_cos_kv
+    xk = apply_rotary_emb(xk, freqs_cos_kv, freqs_sin_kv)
+
+    # grouped multiquery attention: expand out keys and values
+    xk = repeat_kv(xk, self.n_rep)
+    xv = repeat_kv(xv, self.n_rep)
+
+    # make heads into a batch dimension
+    xq = xq.swapaxes(1, 2)  # (bs, n_heads, seqlen_q, head_dim)
+    xk = xk.swapaxes(1, 2)
+    xv = xv.swapaxes(1, 2)
+
+    scores = jnp.matmul(xq, xk.swapaxes(2, 3)) / math.sqrt(self.head_dim)
+
+    # Assuming attn_mask has shape (seqlen_q, seqlen_kv)
+    attn_mask = attn_mask[None, None]
+    scores = (
+          scores + attn_mask
+      )  # (bs, n_heads, seqlen_q, seqlen_kv)
+
+    scores = nn.softmax(scores, axis=-1)
+    if self.dropout_rate > 0.0:
+      scores = self.attn_dropout(scores, deterministic=deterministic)
+    output = jnp.matmul(scores, xv)  # (bs, n_heads, seqlen_q, head_dim)
+
+    # restore time as batch dimension and concat heads
+    output = output.swapaxes(1, 2).reshape(bsz, seqlen_q, -1)
+
+    # final projection into the residual stream
+    output = self.wo(output)
+    if self.dropout_rate > 0.0:
+      output = self.resid_dropout(output, deterministic=deterministic)
+    return output
 
 class CausalAttention(nn.Module):
   """Attention layer that is part of each Transformer layer."""
@@ -332,17 +439,34 @@ class GenericTransformerLayer(nn.Module):
 
   @nn.compact
   def __call__(self, q: jnp.ndarray, kv: jnp.ndarray, mask: jnp.ndarray,
-               deterministic: bool) -> jnp.ndarray:
+               freqs_cos: jnp.ndarray, freqs_sin: jnp.ndarray,
+               freqs_cos_kv: jnp.ndarray=None, freqs_sin_kv: jnp.ndarray=None,
+               deterministic: bool=True) -> jnp.ndarray:
       
-    attention_output = CausalAttention(
-        hidden_size=self.hidden_size,
-        hidden_dropout_prob=self.hidden_dropout_prob,
-        num_attention_heads=self.num_attention_heads,
-        attention_probs_dropout_prob=self.attention_probs_dropout_prob,
-        initializer_fn=self.initializer_fn)(
-            q=q, kv=kv, attention_mask=mask,
+    # attention_output = CausalAttention(
+    #     hidden_size=self.hidden_size,
+    #     hidden_dropout_prob=self.hidden_dropout_prob,
+    #     num_attention_heads=self.num_attention_heads,
+    #     attention_probs_dropout_prob=self.attention_probs_dropout_prob,
+    #     initializer_fn=self.initializer_fn)(
+    #         q=q, kv=kv, attention_mask=mask,
+    #         deterministic=deterministic)
+
+    # Note that this function from MD4 applies dropout
+    # but not the residual connection and layer norm
+    attention_output = MaskedAttentionWithRoPE(
+        dim=self.hidden_size,
+        n_heads=self.num_attention_heads,
+        dropout_rate=self.attention_probs_dropout_prob,
+        qkv_bias=True)(q, kv, attn_mask,
+            freqs_cos, freqs_sin, 
+            freqs_cos_kv=freqs_cos_kv, freqs_sin_kv=freqs_sin_kv,
             deterministic=deterministic)
       
+    # Apply layer norm and residual connection
+    attention_output = nn.LayerNorm(
+        epsilon=LAYERNORM_EPSILON, name='attention_output_ln')(attention_output + q)
+
     layer_output = Mlp(
         hidden_size=self.hidden_size,
         hidden_dropout_prob=self.hidden_dropout_prob,
@@ -381,11 +505,22 @@ class HollowTransformer(nn.Module):
         hidden_dropout_prob=self.hidden_dropout_prob,
         vocab_size=self.vocab_size,
         max_position_embeddings=self.max_position_embeddings + 2, # Including the padded values
-        initializer_fn=truncated_normal(self.initializer_range))(
+        initializer_fn=truncated_normal(self.initializer_range),
+        use_position_embeddings=False)( # No positional embeddings because of the rotary position encoding
             input_ids=input_ids, deterministic=deterministic)
     
     H = self.num_attention_heads
       
+    freqs_cos, freqs_sin = precompute_freqs_cis(args.dim // args.n_heads, seqlen)
+    freqs_cos, freqs_sin = freqs_cos[:L], freqs_sin[:L]
+
+    # Offset the two streams and initialize the mixed stream to None
+    freqs_cos_f, freqs_sin_f = freqs_cos[:-2], freqs_sin[:-2]
+    freqs_cos_b, freqs_sin_b = freqs_cos[2:], freqs_sin[2:]
+    freqs_cos_m, freqs_sin_m = freqs_cos[1:-1], freqs_sin[1:-1]
+    freqs_cos_m_kv = jnp.concatenate([freqs_cos_f, freqs_cos_b], axis=0)
+    freqs_sin_m_kv = jnp.concatenate([freqs_sin_f, freqs_sin_b], axis=0)
+
     forward_mask = jnp.tile(jnp.tril(jnp.ones((L, L)))[None, None], (B, H, 1, 1))
     backward_mask = jnp.tile(jnp.triu(jnp.ones((L, L)))[None, None], (B, H, 1, 1))
     mixing_mask = jnp.concatenate([forward_mask, backward_mask], axis=-1)   
@@ -409,20 +544,27 @@ class HollowTransformer(nn.Module):
     #       num_attention_heads=self.num_attention_heads,
     #       attention_probs_dropout_prob=self.attention_probs_dropout_prob,
     #       initializer_fn=truncated_normal(self.initializer_range))
-      xf = fb_layer(q=xf, kv=xf, mask=forward_mask, deterministic=deterministic)
-      xb = fb_layer(q=xb, kv=xb, mask=backward_mask, deterministic=deterministic)
+      xf = fb_layer(q=xf, kv=xf, mask=forward_mask, 
+                    freqs_cos=freqs_cos_f, freqs_sin=freqs_sin_f,
+                    deterministic=deterministic)
+      xb = fb_layer(q=xb, kv=xb, mask=backward_mask, 
+                    freqs_cos=freqs_cos_b, freqs_sin=freqs_sin_b,
+                    deterministic=deterministic)
 
       if (i + 1) % self.num_layers_per_mixed == 0:
         xm += xf + xb
         xfb = jnp.concatenate([xf, xb], axis=1)
         m_layer = GenericTransformerLayer(
           intermediate_size=self.intermediate_size,
-          hidden_size=self.hidden_size, # since we're combining the streams
+          hidden_size=self.hidden_size,
           hidden_dropout_prob=self.hidden_dropout_prob,
           num_attention_heads=self.num_attention_heads,
           attention_probs_dropout_prob=self.attention_probs_dropout_prob,
           initializer_fn=truncated_normal(self.initializer_range))
-        xm = m_layer(q=xm, kv=xfb, mask=mixing_mask, deterministic=deterministic)
+        xm = m_layer(q=xm, kv=xfb, mask=mixing_mask, 
+                    freqs_cos=freqs_cos_m, freqs_sin=freqs_sin_m,
+                    freqs_cos_kv=freqs_cos_m_kv, freqs_sin_kv=freqs_sin_m_kv,
+                    deterministic=deterministic)
 
     layer_output = xm
       
