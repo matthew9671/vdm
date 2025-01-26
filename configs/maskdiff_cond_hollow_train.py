6608537d85b46461f1b99fb8d0c65f9f3e8be8e1
yixiuz <yixiuz@stanford.edu>
2025-01-26 08:48:37 +0000

    Testing sampling with temp=1 and k=8

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -94,7 +94,7 @@ def get_config():
     # predictor_cutoff_time=0.25, convergence_steps=100,
     # If set to true, only update masked tokens at the last argmax step
     restricted=False,
-    k = 16,
+    k = 8,
     top_k_temperature=1.,
     maskgit_temperature=6,
   )
