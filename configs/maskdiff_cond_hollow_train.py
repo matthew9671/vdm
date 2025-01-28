b2941f90b4ff675d3757f61ca19b11e3b7a6f872
yixiuz <yixiuz@stanford.edu>
2025-01-28 08:36:17 +0000

    Testing no anneal for temperature=1

--- configs/maskdiff_cond_hollow_train.py
+++ configs/maskdiff_cond_hollow_train.py
@@ -97,7 +97,7 @@ def get_config():
     # If set to true, only update masked tokens at the last argmax step
     restricted=False,
     k = 16,
-    top_k_temperature=3.,
+    top_k_temperature=1.,
     maskgit_temperature=6.,
     anneal_temperature=False,
   )
