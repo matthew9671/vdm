d03b864f369af30a669e244b191d2bef5cf12932
yixiuz <yixiuz@stanford.edu>
2025-05-12 00:56:26 +0000

    what about setting restricted to false?

--- configs/maskdiff_cond_baseline_train.py
+++ configs/maskdiff_cond_baseline_train.py
@@ -83,20 +83,20 @@ def get_config():
   config.sampler = d(
 
     # Use a separate model for the corrector
-    use_corrector_model=False,
+    use_corrector_model=True,
 
     seed=42,
-    num_steps=16, # Cut the number of steps in half due to using correctors
+    num_steps=8, # Cut the number of steps in half due to using correctors
     max_samples=50_000, # Stick with 10k samples for comparison
     # "tau_leaping", "gillespies", "euler", "gibbs", "test_convergence",
-    update_type="maskgit", 
+    update_type="gibbs", 
     tag="",
-    corrector="", corrector_step_size=0.,
+    corrector="gibbs", corrector_step_size=0.,
     corrector_entry_time=0.9,
-    num_corrector_steps=0,
+    num_corrector_steps=1,
 
     # If set to true, only update masked tokens at the last argmax step
-    restricted=True,
+    restricted=False,
     k = 16,
     top_k_temperature=1.,
     maskgit_temperature=8.,
