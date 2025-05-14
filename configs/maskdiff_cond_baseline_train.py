ddacd11639b4c888cf86a82322172c49a7035b10
yixiuz <yixiuz@stanford.edu>
2025-05-14 05:56:19 +0000

    k=4, temp=10.0

--- configs/maskdiff_cond_baseline_train.py
+++ configs/maskdiff_cond_baseline_train.py
@@ -98,7 +98,7 @@ def get_config():
     # If set to true, only update masked tokens at the last argmax step
     restricted=True,
     k = 4,
-    top_k_temperature=5.,
+    top_k_temperature=10.,
     maskgit_temperature=8.,
     # This only controls temperature for k-gibbs
     anneal_temperature=False,
